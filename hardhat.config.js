910903554078da6394502bab9fbb803d4e96c150
peterndiritu <peternngendo@gmail.com>
2025-11-09 16:04:20 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+const { ALCHEMY_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.28",
   networks: {
     polygon: {
-      url: process.env.POLYGON_RPC_URL,
-      accounts: [process.env.DEPLOYER_KEY],
+      url: ALCHEMY_URL || "",
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+    amoy: {
+      url: "https://polygon-amoy.g.alchemy.com/v2/YOUR_API_KEY",
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygon: process.env.POLYGONSCAN_API_KEY || "",
     },
   },
 };
