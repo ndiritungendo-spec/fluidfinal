3c897b53acaffc3c1b440aab465f234be6dea4c3
peterndiritu <peternngendo@gmail.com>
2025-11-09 15:36:20 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    polygon: {
+      url: process.env.POLYGON_RPC_URL,
+      accounts: [process.env.DEPLOYER_KEY],
+    },
+  },
+};
