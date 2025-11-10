a208cc9808d2619701b894282cf95391b4ebd38f
ndiritungendo-spec <ndiritungendo@gmail.com>
2025-11-10 12:39:29 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
+require("@nomicfoundation/hardhat-verify");
 
 module.exports = {
-  solidity: {
-    compilers: [
-      { version: "0.8.20" } // matches your FluidToken.sol pragma
-    ]
-  },
+  solidity: "0.8.20",
   networks: {
     polygon: {
-      url: process.env.ALCHEMY_URL,
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
-    }
-  }
+      url: process.env.ALCHEMY_URL || "https://polygon-mainnet.g.alchemy.com/v2/YOUR_KEY",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygon: process.env.POLYGONSCAN_API_KEY || "YOUR_POLYGONSCAN_API_KEY",
+    },
+  },
 };
